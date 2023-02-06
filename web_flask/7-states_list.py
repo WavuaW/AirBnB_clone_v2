469256d6361bacc15fa39c4566e6fba21f46f39a
WavuaW <wavuamuroka@gmail.com>
2023-02-07 00:03:49 +0300

    satic

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,59 +1,25 @@
 #!/usr/bin/python3
-# script that starts a Flask web application
-
-from models import *
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb')
-def hbnb():
-    return "HBNB"
-
-
-@app.route('/c/<string:s>')
-def c(s):
-    new_s = s.replace("_", " ")
-    return "C {}".format(new_s)
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string:s>')
-def python(s="is cool"):
-    new_s = s.replace("_", " ")
-    return "Python {}".format(new_s)
-
-
-@app.route('/number/<int:n>')
-def number(n):
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    return render_template('5-number.html', num=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', num=n)
-
-
-@app.route('/states_list')
-def states_list():
-    return render_template('7-states_list.html',
-                           states=storage.all("State"))
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(err):
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
